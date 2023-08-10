b6e3fc6de00098e91aaf43b1e96d2381aa09c137
Sourav12061999 <sdunity12061999@gmail.com>
2023-08-10 09:28:53 +0530

    10-aug-23 permutaion-string optimized sol completed

--- NeetCode/Sliding Window/permutation-string.js
+++ NeetCode/Sliding Window/permutation-string.js
@@ -1,38 +1,46 @@
 // ! https://leetcode.com/problems/permutation-in-string/
 
 var checkInclusion = function (s1 = "", s2 = "") {
-  const need = {};
+  const need = new Map();
   for (let i = 0; i < s1.length; i++) {
-    need[s1[i]] = 1 + (need[s1[i]] === undefined ? 0 : need[s1[i]]);
+    need.set(s1[i], 1 + (need.get(s1[i]) || 0));
   }
-  console.log(need);
   const have = {};
+  let counter = 0;
   for (let i = 0; i < s1.length; i++) {
     have[s2[i]] = 1 + (have[s2[i]] === undefined ? 0 : have[s2[i]]);
+    if (have[s2[i]] === need.get(s2[i])) counter++;
   }
-  if (isValid(need, have)) return true;
+  // if (isValid(need, have)) return true;
+  if (counter === need.size) return true;
   let left = 1;
   let right = s1.length;
   while (right < s2.length) {
+    if (have[s2[left - 1]] === need.get(s2[left - 1])) {
+      counter--;
+    }
     if (have[s2[left - 1]]) {
       have[s2[left - 1]]--;
     }
     have[s2[right]] = 1 + (have[s2[right]] || 0);
-    if (isValid(need, have)) return true;
+    if (have[s2[right]] === need.get(s2[right])) counter++;
+    // if (isValid(need, have)) return true;
+    if (counter === need.size) return true;
     left++;
     right++;
   }
   return false;
 };
 
-function isValid(need, have) {
-  for (let key in need) {
-    if (need[key] != have[key]) {
-      return false;
+function isValid(need = new Map(), have) {
+  let flag = true;
+  need.forEach((value, key) => {
+    if (value !== have[key]) {
+      flag = false;
     }
-  }
+  });
 
-  return true;
+  return flag;
 }
 
-console.log(checkInclusion("ab", "eidboaoo"));
+console.log(checkInclusion("ab", "eidbaoo"));
